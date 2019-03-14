8c54a8bb2853dd5dfed755b38fb1aae9d877d930
Craftingexpert1 <dev.velp@gmail.com>
2019-03-13 21:27:48 -0400

    hi

---
+++ index.js
@@ -0,0 +1,95 @@
+var spaghetti = {
+    reverse: function (text, splitter) {
+        var setSplitter = splitter || '';
+        var string = text.split(setSplitter);
+        string = string.reverse();
+        string = string.join(setSplitter);
+        return string;
+    },
+    stir: function (text) {
+        var length = text.length;
+        var arr = text.split('');
+        var stirredString = '';
+        for (var i = 0; i < length; i++) {
+            var number = Math.floor(Math.random() * arr.length);
+            var selected = arr[number];
+            stirredString += selected;
+            arr.splice(number, 1);
+        }
+        return stirredString;
+    },
+    capitalizeFirst: function (text) {
+        var split = text.split(' ');
+        var total = '';
+        split.forEach(function (element) {
+            total += element.charAt(0).toUpperCase() + element.slice(1) + ' ';
+        });
+        total = total.slice(0, -1);
+        return total;
+    },
+    cook: function (text, password, oven_heat_level) {
+        var seed = password.split('');
+        var arr = [];
+        seed.forEach(function (element) {
+            element = element.charCodeAt(0);
+            element *= oven_heat_level;
+            arr.push(element);
+        });
+        seed = arr;
+        var string = text.split('');
+        arr = [];
+        string.forEach(function (element, index) {
+            element = String.fromCharCode(element.charCodeAt(0) + seed[index % seed.length]);
+            arr.push(element);
+        });
+        string = arr.join('');
+        if (oven_heat_level > 10000000000000) {
+            console.warn('WARNING: The requested oven_heat_level is very high. You may have burnt your spaghetti string. Proceed with caution.')
+        }
+        return string;
+    },
+    uncook: function (text, password, oven_heat_level) {
+        var seed = password.split('');
+        var arr = [];
+        seed.forEach(function (element) {
+            element = element.charCodeAt(0);
+            element *= oven_heat_level;
+            arr.push(element);
+        });
+        seed = arr;
+        var string = text.split('');
+        arr = [];
+        string.forEach(function (element, index) {
+            element = String.fromCharCode(element.charCodeAt(0) - seed[index % seed.length]);
+            arr.push(element);
+        });
+        string = arr.join('');
+        return string;
+    },
+    spaghettiString: function (string, key) {
+        this.value = string;
+        Object.defineProperty(this, 'value', {
+            enumerable: true,
+            writable: false
+        });
+        this.heated = true;
+        this.heat = function (heating_key) {
+            if (heating_key == key) {
+                this.heated = true;
+                Object.defineProperty(this, 'value', {
+                    enumerable: true,
+                    writable: true
+                })
+            }
+        };
+        this.freeze = function (freezing_key) {
+            if (freezing_key == key) {
+                this.heated = false;
+                Object.defineProperty(this, 'value', {
+                    enumerable: true,
+                    writable: false
+                })
+            }
+        };
+    }
+};
