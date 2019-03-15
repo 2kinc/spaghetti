8505bca074efaa1fca40cc44888c2d0a955ff8d8
Ken Zhou <mailofrealcev@gmail.com>
2019-03-15 16:04:42 -0400

    good changes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-for (var i = 0; i > document.querySelector('h1, h2, h3, h4, h5, h6').length; i++) {
-    var el = document.querySelector('h1, h2, h3, h4, h5, h6')[i];
+for (var i = 0; i < document.querySelectorAll('h1, h2, h3, h4, h5, h6').length; i++) {
+    var el = document.querySelectorAll('h1, h2, h3, h4, h5, h6')[i];
     el.id = 'header-id-' + i;
-    var text = el.innerHTML;
+    var a = document.createElement('a');
+    a.setAttribute('href', '#header-id-' + i);
+    a.innerText = '::';
+    a.className = 'header-link';
+    el.insertBefore(a, el.firstChild);
 }
