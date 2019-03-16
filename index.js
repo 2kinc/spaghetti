66d5d51d2844ce91b1464c4ed76a1c6d288b088d
Ken Zhou <mailofrealcev@gmail.com>
2019-03-15 22:08:19 -0400

    more stuff incoming

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ for (var i = 0; i < document.querySelectorAll('h1, h2, h3, h4, h5, h6').length; 
 document.querySelector('#actn-btn').addEventListener('click', function() {
 	document.querySelector('#actn-p').innerHTML = spaghetti.stir(spaghetti.cook(spaghetti.stir(spaghetti.cook(document.querySelector('#actn-p').innerHTML.italics(''),'d',Math.random()*100000000)),'s',Math.random()*100000000000))
 });
+
+document.querySelector('#reverse-demo-button').addEventListener('click', function () {
+    document.querySelector('#reverse-demo').innerHTML = spaghetti.reverse(document.querySelector('#reverse-demo').innerHTML);
+});
